544918d1bd77735404a0ca83b5949698ee1bb001
giho.seong <wasd92@naver.com>
2025-04-21 16:47:39 +0900

    Add MO(1) + f

--- keyboards/keychron/k9_pro/ansi/white/keymaps/qmk/keymap.c
+++ keyboards/keychron/k9_pro/ansi/white/keymaps/qmk/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN1] = LAYOUT_61_ansi(
         KC_GRAVE,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     MC_CTRL_U,     KC_I,     KC_O,     MC_CTRL_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-        KC_CAPS,  MC_CTRL_A,     KC_S,     MC_CTRL_D,     KC_F,     KC_HOME,     KC_LEFT,     KC_DOWN,     KC_UP,     KC_RGHT,     KC_END,  KC_QUOT,            KC_ENT,
+        KC_CAPS,  MC_CTRL_A,     KC_S,     MC_CTRL_D,     KC_LSFT,     KC_HOME,     KC_LEFT,     KC_DOWN,     KC_UP,     KC_RGHT,     KC_END,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     MC_CTRL_C,     KC_V,     KC_B,     MC_CTRL_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LOPTN,  KC_LCMMD,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),MO(L_FN1),KC_RCTL),
 
